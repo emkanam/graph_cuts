9ac2ebfeddebfeaf8827b95dd7a54a11a5ced59e
kahanam emmanuel <emkanam@gmail.com>
2020-03-16 11:59:08 +0100

    using distance weights

--- graphCut/graphModel.py
+++ graphCut/graphModel.py
@@ -18,10 +18,10 @@ class GraphModel(object):
         # create edges
         for node in nodes:
             if node not in ["s", "t"]:
-                # compute affinity
+                # compute weights
                 im_xy = self.image[node[0], node[1]]
-                s_weight = np.exp(-(self.s_val-im_xy)**2)
-                t_weight = np.exp(-(self.t_val-im_xy)**2)
+                s_weight = (self.s_val-im_xy)**2
+                t_weight = (self.t_val-im_xy)**2
                 # add edge and weight
                 self.G.add_edge("s", node, weight=s_weight)  # edge from source to current node
                 self.G.add_edge(node, "t", weight=t_weight)  # edge from current to target node
@@ -32,7 +32,7 @@ class GraphModel(object):
                 coord1, coord2 = edge
                 im_xy1 = self.image[coord1[0], coord1[1]]
                 im_xy2 = self.image[coord2[0], coord2[1]]
-                weight = np.exp(-(im_xy1-im_xy2)**2)
+                weight = (im_xy1-im_xy2)**2
                 self.G[coord1][coord2]['weight'] = weight
 
     def cut_graph(self):
